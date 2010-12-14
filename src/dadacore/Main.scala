4cb1fdecbe626f0d486b7352eaec28ae52e7e9c7
kolen <incredible.angst@gmail.com>
2010-12-15 02:55:51 +0300

    Test main function.

--- src/dadacore/Main.scala
+++ src/dadacore/Main.scala
@@ -1,17 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package dadacore
+
+import brain.Brain
 
 object Main {
   /**
    * @param args the command line arguments
    */
   def main(args: Array[String]): Unit = {
-    import dadacore.storage
+    val br = new Brain
+    br.learn("Шульгин ваще сверхчеловек, почти все феники которые щас жрут это он придумал.")
+    br.learn("купила вчера новог. украшение-ветку ели с красными яблоками и клюквой.и только сегодня доперла что на елях яблоки не растут.опять наебали.")
+    br.learn("у меня в закладках рома жолудь и тёлка, у которой в альбоме \"альбомчик маленькой готэссы\"")
+    br.learn("Чирок-свистунок взлетает вертикально, почти все чирки взлетают вертикально")
 
-    println("Hello, world!")
+    for (i <- 0 until 100)
+      println(br.generateRandom)
   }
 }
