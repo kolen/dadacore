589c9b7ddd447afb5cdb565d294904dd9fe6a8b0
kolen <incredible.angst@gmail.com>
2010-12-14 16:49:44 +0300

    Added Composer.

---
+++ src/dadacore/composer/Composer.scala
@@ -0,0 +1,23 @@
+package dadacore.composer
+
+import annotation.tailrec
+import dadacore.storage._
+
+class ComposerException extends Exception
+
+object Composer {
+  def random(conn:StorageConnection):List[String] = {
+    @tailrec
+    def traverse(existing_words:List[String], state:State):List[String] = {
+      state match {
+        case s:WordState => traverse(s.words.last :: existing_words, s.next_random)
+        case _:EndingState => existing_words
+      }
+    }
+
+    conn.starting_state.next_random match {
+      case s:WordState => traverse(s.words, s)
+      case _ => throw new ComposerException()
+    }
+  }
+}
