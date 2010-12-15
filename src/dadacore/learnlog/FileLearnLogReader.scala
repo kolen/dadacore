dc1ed5e7849eea3a427eaacd6e1f2d8a21507b98
kolen <incredible.angst@gmail.com>
2010-12-16 02:59:09 +0300

    Made LearnLogReader iterator, not traversable, changed FileLearnLogReader.

--- src/dadacore/learnlog/FileLearnLogReader.scala
+++ src/dadacore/learnlog/FileLearnLogReader.scala
@@ -7,25 +7,24 @@ import java.text.{SimpleDateFormat}
 
 class FileLearnLogReader(filename:String) extends LearnLogReader {
   private val date_format = new SimpleDateFormat("yyyy-MM-dd,hh:mm")
-  private var br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))
+  private val input_iterator = io.Source.fromFile(filename, "UTF-8").getLines()
 
-  def foreach[U](f: (LearnLogElement) => U) = {
-    def parseLine(line:String): LearnLogElement =
-      line.split(" ", 4).toList match {
-        case List(datestr, user, source, text) =>
-          val date = date_format.parse(datestr)
-          new LearnLogElement(date, user, source, text)
-        case _ => throw new LearnLogInvalidLineException(line)
-      }
-
-    @tailrec
-    def readLine:Unit = br.readLine match {
-      case line:String =>
-        f(parseLine(line))
-        readLine
-      case null => Nil
+  private def parseLine(line:String): LearnLogElement =
+    line.split(" ", 4).toList match {
+      case List(datestr, user, source, text) =>
+        val date = date_format.parse(datestr)
+        new LearnLogElement(date, user, source, text)
+      case _ => throw new LearnLogInvalidLineException(line)
     }
-  }
 
-
+  def hasNext = input_iterator.hasNext
+  def next() = parseLine(input_iterator.next)
 }
+
+// java.lang.AssertionError: expected:
+// <List(LearnLogElement Mon Jan 16 10:56:00 MSK 3911 mithgol wikipedia,2ch Понятие шушпанчика зародилось в сети Фидонет в 2001 году, его происхождение связано с именем одного из пойнтов этой сети, LearnLogElement Tue Jan 17 11:23:00 MSK 3911 ziggol goatse.cx Сын, почему от тебя пахнет зигами? - Ты опять на Манежную площадь ходил? - Не, мам, просто у метро назиговано было!)>
+// <List(LearnLogElement Thu Dec 16 10:56:00 MSK 2010 mithgol wikipedia,2-ch Понятие шушпанчика зародилось в сети Фидонет в 2001 году, его происхождение связано с именем одного из пойнтов этой сети, LearnLogElement Fri Dec 17 11:23:00 MSK 2010 ziggol goatse.cx Сын, почему от тебя пахнет зигами? - Ты опять на Манежную площадь ходил? - Не, мам, просто у метро назиговано было!)>
+
+//java.lang.AssertionError: expected:
+// <List(LearnLogElement Mon Jan 16 10:56:00 MSK 3911 mithgol wikipedia,2-ch Понятие шушпанчика зародилось в сети Фидонет в 2001 году, его происхождение связано с именем одного из пойнтов этой сети, LearnLogElement Tue Jan 17 11:23:00 MSK 3911 ziggol goatse.cx Сын, почему от тебя пахнет зигами? - Ты опять на Манежную площадь ходил? - Не, мам, просто у метро назиговано было!)> but was:
+// <List(LearnLogElement Thu Dec 16 10:56:00 MSK 2010 mithgol wikipedia,2-ch Понятие шушпанчика зародилось в сети Фидонет в 2001 году, его происхождение связано с именем одного из пойнтов этой сети, LearnLogElement Fri Dec 17 11:23:00 MSK 2010 ziggol goatse.cx Сын, почему от тебя пахнет зигами? - Ты опять на Манежную площадь ходил? - Не, мам, просто у метро назиговано было!)>
--- src/dadacore/learnlog/LearnLogReader.scala
+++ src/dadacore/learnlog/LearnLogReader.scala
@@ -2,4 +2,4 @@ package dadacore.learnlog
 
 class LearnLogInvalidLineException(message:String) extends Exception
 
-trait LearnLogReader extends Traversable[LearnLogElement]
+trait LearnLogReader extends Iterator[LearnLogElement]
