b3107d882e26d399f12b6709f487ad043880b708
kolen <incredible.angst@gmail.com>
2010-12-16 02:15:35 +0300

    Parse date format.

--- src/dadacore/learnlog/FileLearnLogReader.scala
+++ src/dadacore/learnlog/FileLearnLogReader.scala
@@ -3,16 +3,17 @@ package dadacore.learnlog
 import java.io.{FileInputStream, InputStreamReader, BufferedReader}
 import annotation.tailrec
 import java.util.Date
+import java.text.{SimpleDateFormat}
 
 class FileLearnLogReader(filename:String) extends LearnLogReader {
+  private val date_format = new SimpleDateFormat("yyyy-MM-dd,hh:mm")
   private var br = new BufferedReader(new InputStreamReader(new FileInputStream(filename)))
-
 
   def foreach[U](f: (LearnLogElement) => U) = {
     def parseLine(line:String): LearnLogElement =
       line.split(" ", 4).toList match {
         case List(datestr, user, source, text) =>
-          val date = new Date(datestr)
+          val date = date_format.parse(datestr)
           new LearnLogElement(date, user, source, text)
         case _ => throw new LearnLogInvalidLineException(line)
       }
