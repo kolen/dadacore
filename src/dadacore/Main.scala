3a4682a54de07b1586835a0f2faf4ef365fbd20f
kolen <incredible.angst@gmail.com>
2010-12-16 14:01:41 +0300

    Read file and generate 100 lines in main.

--- src/dadacore/Main.scala
+++ src/dadacore/Main.scala
@@ -7,11 +7,12 @@ object Main {
    * @param args the command line arguments
    */
   def main(args: Array[String]): Unit = {
+    val input_iterator = io.Source.fromFile(args(0), "UTF-8").getLines()
+
     val br = new Brain
-    br.learn("Шульгин ваще сверхчеловек, почти все феники которые щас жрут это он придумал.")
-    br.learn("купила вчера новог. украшение-ветку ели с красными яблоками и клюквой.и только сегодня доперла что на елях яблоки не растут.опять наебали.")
-    br.learn("у меня в закладках рома жолудь и тёлка, у которой в альбоме \"альбомчик маленькой готэссы\"")
-    br.learn("Чирок-свистунок взлетает вертикально, почти все чирки взлетают вертикально")
+    for (line <- input_iterator) {
+      br.learn(line)
+    }
 
     for (i <- 0 until 100)
       println(br.generateRandom)
