0d9c13aae16216d577b6879eb2b09162f8ee7cf5
kolen <incredible.angst@gmail.com>
2010-12-16 13:51:34 +0300

    Capitalize after "?" and "!" in addition to ".".

--- src/dadacore/text/Joiner.scala
+++ src/dadacore/text/Joiner.scala
@@ -3,7 +3,7 @@ package dadacore.text
 object Joiner {
   def join(words:List[String]):String = {
     val out = new StringBuilder()
-    var last_was_point = true
+    var last_was_end_of_sentence = true
     var last_was_word = false
     for (word <- words) {
       if (word charAt 0 isLetterOrDigit) {
@@ -12,27 +12,26 @@ object Joiner {
           out append ' '
         }
 
-        if (last_was_point) {
+        if (last_was_end_of_sentence) {
           out append word.charAt(0).toUpper
           out append word.substring(1)
         } else {
           out append word
         }
 
-        last_was_point = false
+        last_was_end_of_sentence = false
         last_was_word = true
       } else {
         // punctuation chars
         out append word
 
-        last_was_point = word != "." && (word.indexOf('.') != -1)
+        last_was_end_of_sentence = word != "." && (word.matches("[?!.]"))
         last_was_word = false
       }
     }
 
-    if (!words.last.matches("[?!.]")) {
+    if (last_was_word)
       out append '.'
-    }
 
     out.toString
   }
