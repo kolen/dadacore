1014882a707928f3212d4f71338c40205ef8669d
kolen <incredible.angst@gmail.com>
2010-12-18 02:43:20 +0300

    LearnLogWriter: define helper methods in trait itself.

--- src/dadacore/learnlog/LearnLogWriter.scala
+++ src/dadacore/learnlog/LearnLogWriter.scala
@@ -3,7 +3,9 @@ package dadacore.learnlog
 import java.util.Date
 
 trait LearnLogWriter {
-  def write(user:String, source:String, text:String)
-  def write(date:Date, user:String, source:String, text:String)
-  def write(log_element:LearnLogElement)
+  def write(user:String, source:String, text:String):Unit =
+    write(new Date(), user, source, text)
+  def write(date:Date, user:String, source:String, text:String):Unit =
+    write(new LearnLogElement(date, user, source, text))
+  def write(log_element:LearnLogElement):Unit
 }
