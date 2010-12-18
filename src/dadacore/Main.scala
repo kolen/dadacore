2fe6b9e48a734b5807806d007f4d008a66e5b057
kolen <incredible.angst@gmail.com>
2010-12-18 03:20:58 +0300

    Logging in Brain; updated main to use it.

--- src/dadacore/Main.scala
+++ src/dadacore/Main.scala
@@ -7,11 +7,13 @@ object Main {
    * @param args the command line arguments
    */
   def main(args: Array[String]): Unit = {
-    val input_iterator = io.Source.fromFile(args(0), "UTF-8").getLines()
-
     val br = new Brain
-    for (line <- input_iterator) {
-      br.learn(line)
+
+    if (args.length == 1) {
+      val input_iterator = io.Source.fromFile(args(0), "UTF-8").getLines()
+      for (line <- input_iterator) {
+        br.learn(line)
+      }
     }
 
     for (i <- 0 until 100)
