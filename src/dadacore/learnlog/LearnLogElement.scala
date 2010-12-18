4a57c256caabc7e463295885d00f104cb7ac5951
kolen <incredible.angst@gmail.com>
2010-12-18 03:08:45 +0300

    Removed commented compare code.

--- src/dadacore/learnlog/LearnLogElement.scala
+++ src/dadacore/learnlog/LearnLogElement.scala
@@ -3,16 +3,5 @@ package dadacore.learnlog
 import java.util.Date
 
 case class LearnLogElement(date:Date, user:String, source:String, text:String) {
-//  override def equals(obj: AnyRef) =
-//    obj match {
-//      case that:LearnLogElement =>
-//        date == that.date &&
-//        user == that.user &&
-//        source == that.source &&
-//        text == that.text
-//      case _ =>
-//        false
-//    }
-
   override def toString = "LearnLogElement " + date.toString + " " + user + " " + source + " " + text
 }
