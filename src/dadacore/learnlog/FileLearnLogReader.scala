e0ba5cf367f29e8549213a17918161688b0db72e
kolen <incredible.angst@gmail.com>
2010-12-18 03:20:23 +0300

    FileLearnLogReader: check if file don't exist, throw LearnLogNoSuchLogError.

--- src/dadacore/learnlog/FileLearnLogReader.scala
+++ src/dadacore/learnlog/FileLearnLogReader.scala
@@ -1,10 +1,16 @@
 package dadacore.learnlog
 
 import java.text.SimpleDateFormat
+import java.io.FileNotFoundException
 
 class FileLearnLogReader(filename:String) extends LearnLogReader {
   private val date_format = new SimpleDateFormat("yyyy-MM-dd,hh:mm")
-  private val input_iterator = io.Source.fromFile(filename, "UTF-8").getLines()
+  private var input_iterator:Iterator[String] = null
+  try {
+    input_iterator = io.Source.fromFile(filename, "UTF-8").getLines()
+  } catch {
+    case e:FileNotFoundException => throw new LearnLogNoSuchLogError(filename)
+  }
 
   private def parseLine(line:String): LearnLogElement =
     line.split(" ", 4).toList match {
