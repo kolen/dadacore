f85dbd26cfcdf67184cf242c2e58067f26006b2a
kolen <incredible.angst@gmail.com>
2011-03-21 14:41:54 +0300

    Added scalatra to dependencies.

---
+++ src/dadacore/web/servlet.scala
@@ -0,0 +1,11 @@
+package dadacore.web
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: kolen
+ * Date: 21.03.11
+ * Time: 14:27
+ * To change this template use File | Settings | File Templates.
+ */
+
+class servlet
