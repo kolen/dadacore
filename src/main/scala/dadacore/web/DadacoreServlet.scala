f9bfbfe7c93f8a61c53bcd99ad4ba534621d6910
kolen <incredible.angst@gmail.com>
2011-03-22 15:20:54 +0300

    Generate multiple lines at main page.

--- src/main/scala/dadacore/web/DadacoreServlet.scala
+++ src/main/scala/dadacore/web/DadacoreServlet.scala
@@ -3,31 +3,14 @@ package dadacore.web
 import org.scalatra.ScalatraServlet
 import dadacore.brain.Brain
 
-/*
-object Main {
-  /**
-   * @param args the command line arguments
-   */
-  def main(args: Array[String]): Unit = {
-    val br = new Brain
-
-    if (args.length == 1) {
-      val input_iterator = io.Source.fromFile(args(0), "UTF-8").getLines()
-      for (line <- input_iterator) {
-        br.learn(line)
-      }
-    }
-
-    for (i <- 0 until 100)
-      println(br.generateRandom)
-  }
-}
-*/
-
 class DadacoreServlet extends ScalatraServlet {
   val brain = new Brain
 
   get("/") {
-    brain.generateRandom
+    contentType = "text/html"
+    val wr = response.getWriter()
+    for (i <- 0 until 50) {
+      wr.append((<p>{brain.generateRandom}</p>).toString)
+    }
   }
 }
