efed729d0ca275d98c3efb85a29875254a85e2af
kolen <incredible.angst@gmail.com>
2012-10-13 02:47:06 +0400

    Implemented randomUniform for PossibleNextWords

--- src/main/scala/dadacore/model/PossibleNextWords.scala
+++ src/main/scala/dadacore/model/PossibleNextWords.scala
@@ -1,6 +1,8 @@
 package dadacore.model
 
+import util.Random
+
 class PossibleNextWords[Word] (val words:IndexedSeq[NextWordEntry[Word]]) {
-  //def randomUniform(): NextWordEntry[Word] TODO: implement randomUniform
+  def randomUniform(): NextWordEntry[Word] = words(Random.nextInt(words.length))
   //def randomModel(): NextWordEntry[Word] TODO: implement randomModel
 }
