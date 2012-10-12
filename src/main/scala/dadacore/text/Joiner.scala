acadb00b89df174c5c2e2d17a556b468c0e28358
kolen <incredible.angst@gmail.com>
2012-10-13 02:21:10 +0400

    Joiner remade for new behavior, where spaces are stored and not need to be outputted

--- src/main/scala/dadacore/text/Joiner.scala
+++ src/main/scala/dadacore/text/Joiner.scala
@@ -6,16 +6,11 @@ object Joiner {
   private val sentence_end_regex = new Regex("[?!.\u2026]")
 
   def join(words:Seq[String]):String = {
+    // TODO: rewrite, some code left from times when joiner added spaces between words
     val out = new StringBuilder()
     var last_was_end_of_sentence = true
-    var last_was_word = false
     for (word <- words) {
       if (word.charAt(0).isLetterOrDigit) {
-        // word
-        if (last_was_word) {
-          out append ' '
-        }
-
         if (last_was_end_of_sentence) {
           out append word.charAt(0).toUpper
           out append word.substring(1)
@@ -24,13 +19,11 @@ object Joiner {
         }
 
         last_was_end_of_sentence = false
-        last_was_word = true
       } else {
         // punctuation chars
         out append word
 
         last_was_end_of_sentence = sentence_end_regex.findFirstIn(word).isDefined
-        last_was_word = false
       }
     }
 
